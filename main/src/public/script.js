c3c7b9cd17e507daaba84d50d66d34d770f3e9d9
boss uengsamphanpong <ronrat@bu.edu>
2020-11-05 07:05:31 +0700

    merging both side of code

--- main/src/public/script.js
+++ main/src/public/script.js
@@ -27,30 +27,30 @@ navigator.mediaDevices.getUserMedia({
     audio: true
 }).then(stream => {
     addVideoStream(myVideo, stream, 0)
-    var btn = document.createElement("BUTTON");
-    btn.innerHTML = "1080p"; 
-    btn.addEventListener('click', function (){
-        changeres("1080px", "1920");
-    });
-    var btn2 = document.createElement("BUTTON");
-    btn2.innerHTML = "240p"; 
-    btn2.addEventListener('click', function (){
-        changeres("240px", "320");
-    });
-    var btn3 = document.createElement("BUTTON");
-    btn3.innerHTML = "480p"; 
-    btn3.addEventListener('click', function (){
-        changeres("480px", "640");
-    });
-    var btn4 = document.createElement("BUTTON");
-    btn4.innerHTML = "720p"; 
-    btn4.addEventListener('click', function (){
-        changeres("720px", "1280");
-    });
-    document.getElementById('buttons').append(btn2);
-    document.getElementById('buttons').append(btn3);
-    document.getElementById('buttons').append(btn4);
-    document.getElementById('buttons').append(btn);
+    // var btn = document.createElement("BUTTON");
+    // btn.innerHTML = "1080p"; 
+    // btn.addEventListener('click', function (){
+    //     changeres("1080px", "1920");
+    // });
+    // var btn2 = document.createElement("BUTTON");
+    // btn2.innerHTML = "240p"; 
+    // btn2.addEventListener('click', function (){
+    //     changeres("240px", "320");
+    // });
+    // var btn3 = document.createElement("BUTTON");
+    // btn3.innerHTML = "480p"; 
+    // btn3.addEventListener('click', function (){
+    //     changeres("480px", "640");
+    // });
+    // var btn4 = document.createElement("BUTTON");
+    // btn4.innerHTML = "720p"; 
+    // btn4.addEventListener('click', function (){
+    //     changeres("720px", "1280");
+    // });
+    // document.getElementById('buttons').append(btn2);
+    // document.getElementById('buttons').append(btn3);
+    // document.getElementById('buttons').append(btn4);
+    // document.getElementById('buttons').append(btn);
 
     // people in the room is calling us
     myPeer.on('call', call => {
@@ -96,9 +96,9 @@ myPeer.on('connection', function (conn) {
     conn.on('data', function (data) {
         console.log(data);
         // do stuff to "data"
+        append_message("remote", data)
         // document.getElementById("text_message").innerText = data
-        document.getElementById("text").innerHTML = data
-        // 
+        // document.getElementById("text").innerHTML = data
     });
     out_conn_text = conn
 });
@@ -177,15 +177,39 @@ function connect_text(userId) {
     out_conn_text.on('data', function (data) {
         // do stuff with "data"
         console.log(data);
+        append_message("remote", data)
         // document.getElementById("text_message").innerText = data
-        document.getElementById("text").innerHTML= data
+        // document.getElementById("text").innerHTML= data
     });
 }
 
 function send_message() {
     var message = document.getElementById("chat_text_input").value;
+    // only send if the input is not empty
+    if (message != "") {
+        document.getElementById("chat_text_input").value = ""
+        out_conn_text.send(message);
+        append_message("local", message)
+    }
+
     document.getElementById("chat_text_input").value = ""
     out_conn_text.send(message);
+}
+
+function append_message(name, message) {
+    // Find a <table> element with id="message_board":
+    var table = document.getElementById("message_board");
+
+    // Create an empty <tr> element and add it to the end of the table:
+    var row = table.insertRow(-1);
+
+    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+    // Add some text to the new cells:
+    cell1.innerHTML = name;
+    cell2.innerHTML = message;
 }
 
 function changeres(h, w){
