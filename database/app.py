8cae3b51de613e3ba01532ebd48e0f5ea5375a94
boss uengsamphanpong <ronrat@bu.edu>
2020-12-07 17:53:40 +0700

    add debug print

--- database/app.py
+++ database/app.py
@@ -225,5 +225,6 @@ def update_token(username, token):
 
 
 if __name__ == "__main__":
+    print("running main")
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
